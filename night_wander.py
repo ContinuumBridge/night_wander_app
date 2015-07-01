f8256d3410f81b37c23ee0486d2da77ada7a3ed1
PeterClaydon <peter.claydon@continuumbridge.com>
2015-07-01 14:42:11 +0100

    First tested version

--- night_wander.py
+++ night_wander.py
@@ -6,11 +6,7 @@
 
 # Default values:
 config = {
-    "night_wandering": "True",
-    "night_start": "00:30",
-    "night_end": "23:00",
-    "night_ignore_time": 15,
-    "cid": "none"
+    "night_wandering": "False"
 }
 
 import sys
@@ -25,8 +21,8 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-CONFIG_FILE                       = CB_CONFIG_DIR + "sch2_app.config"
-CID                               = "CID71"  # Client ID
+CONFIG_FILE                       = CB_CONFIG_DIR + "night_wander.config"
+CID                               = "CID164"  # Client ID
 
 def betweenTimes(t, t1, t2):
     # True if epoch t is between times of day t1 and t2 (in 24-hour clock format: "23:10")
@@ -57,34 +53,64 @@ def betweenTimes(t, t1, t2):
         decision = False
     return decision
 
+def nicetime(timeStamp):
+    localtime = time.localtime(timeStamp)
+    milliseconds = '%03d' % int((timeStamp - int(timeStamp)) * 1000)
+    now = time.strftime('%H:%M:%S, %d-%m-%Y', localtime)
+    return now
+
 class NightWander():
-    def __init__(self, aid, config):
-        self.config = config
-        self.aid = aid
+    def __init__(self, bridge_id):
+        self.bridge_id = bridge_id
         self.lastActive = 0
         self.activatedSensors = []
+        self.s = []
+        self.waiting = False
 
     def setNames(self, idToName):
         self.idToName = idToName
 
     def onChange(self, devID, timeStamp, value):
-        self.cbLog("debug", "Night Wander onChange, devID: " + devID + " value: " + value)
+        self.cbLog("debug", "onChange, devID: " + devID + " value: " + value)
         if value == "on":
-            alarm = betweenTimes(timeStamp, self.config["night_start"], self.config["night_end"])
-            if alarm:
+            alert = betweenTimes(timeStamp, config["night_start"], config["night_end"])
+            self.cbLog("debug", "alert: " + str(alert))
+            if alert:
                 sensor = self.idToName[devID]
+                self.cbLog("debug", "sensor: " + sensor)
                 if sensor not in self.activatedSensors:
                     self.activatedSensors.append(self.idToName[devID])
-                if timeStamp - self.lastActive > self.config["night_ignore_time"]:
-                    self.cbLog("debug", "Night Wander: " + str(alarm) + ": " + str(time.asctime(time.localtime(timeStamp))) + \
+                if timeStamp - self.lastActive > config["night_ignore_time"]:
+                    self.cbLog("debug", "Night Wander: " + str(alert) + ": " + str(time.asctime(time.localtime(timeStamp))) + \
                     " sensors: " + str(self.activatedSensors))
-                    msg = {"m": "alarm",
-                           "s": str(", ".join(self.activatedSensors)),
+                    msg = {"m": "alert",
+                           "a": "Night wandering detected by " + str(", ".join(self.activatedSensors)) + " at " + nicetime(timeStamp),
                            "t": timeStamp
                           }
                     self.client.send(msg)
+                    self.cbLog("debug", "msg send to client: " + str(json.dumps(msg, indent=4)))
                     self.lastActive = timeStamp
                     self.activatedSensors = []
+                    values = {
+                        "name": self.bridge_id + "/Night_Wander/" + sensor,
+                        "points": [[int(timeStamp*1000), 1]]
+                    }
+                    self.storeValues(values)
+
+    def sendValues(self):
+        msg = {"m": "data",
+               "d": self.s
+               }
+        self.cbLog("debug", "sendValues. Sending: " + str(json.dumps(msg, indent=4)))
+        self.client.send(msg)
+        self.s = []
+        self.waiting = False
+
+    def storeValues(self, values):
+        self.s.append(values)
+        if not self.waiting:
+            self.waiting = True
+            reactor.callLater(config["data_send_delay"], self.sendValues)
 
 class App(CbApp):
     def __init__(self, argv):
@@ -110,29 +136,42 @@ class App(CbApp):
         self.sendManagerMessage(msg)
 
     def onConcMessage(self, message):
+        #self.cbLog("debug", "onConcMessage, message: " + str(json.dumps(message, indent=4)))
+        if "status" in message:
+            if message["status"] == "ready":
+                # Do this after we have established communications with the concentrator
+                msg = {
+                    "m": "req_config",
+                    "d": self.id
+                }
+                self.client.send(msg)
         self.client.receive(message)
 
     def onClientMessage(self, message):
-        self.cbLog("debug", "onClientMessage, message: " + str(json.dumps(message, indent=4)))
+        #self.cbLog("debug", "onClientMessage, message: " + str(json.dumps(message, indent=4)))
         global config
         if "config" in message:
-            config = message["config"]
-            #self.cbLog("debug", "onClientMessage, updated UUIDs: " + str(json.dumps(config, indent=4)))
-            try:
-                with open(CONFIG_FILE, 'w') as f:
-                    json.dump(config, f)
-            except Exception as ex:
-                self.cbLog("warning", "onClientMessage, could not write to file. Type: " + str(type(ex)) + ", exception: " +  str(ex.args))
-            self.readLocalConfig()
-            self.requestUUIDs(self.beaconAdaptor)
+            if "warning" in message["config"]:
+                self.cbLog("warning", "onClientMessage: " + str(json.dumps(message["config"], indent=4)))
+            else:
+                try:
+                    config = message["config"]
+                    with open(CONFIG_FILE, 'w') as f:
+                        json.dump(config, f)
+                    self.cbLog("info", "Config updated")
+                except Exception as ex:
+                    self.cbLog("warning", "onClientMessage, could not write to file. Type: " + str(type(ex)) + ", exception: " +  str(ex.args))
+                self.readLocalConfig()
 
     def onAdaptorData(self, message):
-        self.cbLog("debug", "onAdaptorData, message: " + str(json.dumps(message, indent=4)))
+        #self.cbLog("debug", "onAdaptorData, message: " + str(json.dumps(message, indent=4)))
         if message["characteristic"] == "binary_sensor":
             self.nightWander.onChange(message["id"], message["timeStamp"], message["data"])
 
     def onAdaptorService(self, message):
-        self.cbLog("debug", "onAdaptorService, message: " + str(json.dumps(message, indent=4)))
+        #self.cbLog("debug", "onAdaptorService, message: " + str(json.dumps(message, indent=4)))
+        if self.state == "starting":
+            self.setState("running")
         self.devServices.append(message)
         serviceReq = []
         for p in message["service"]:
@@ -144,18 +183,16 @@ class App(CbApp):
                "service": serviceReq}
         self.sendMessage(msg, message["id"])
         self.cbLog("debug", "onAdaptorService, response: " + str(json.dumps(msg, indent=4)))
-        self.setState("running")
 
     def readLocalConfig(self):
         global config
         try:
             with open(CONFIG_FILE, 'r') as f:
                 newConfig = json.load(f)
-                self.cbLog("debug", "Read sch2_app.config")
+                self.cbLog("debug", "Read local config")
                 config.update(newConfig)
         except Exception as ex:
-            self.cbLog("warning", "sch2_app.config does not exist or file is corrupt")
-            self.cbLog("warning", "Exception: " + str(type(ex)) + str(ex.args))
+            self.cbLog("warning", "LoCAL config does not exist or file is corrupt. Exception: " + str(type(ex)) + str(ex.args))
         for c in config:
             if c.lower in ("true", "t", "1"):
                 config[c] = True
@@ -180,7 +217,7 @@ class App(CbApp):
         self.client.onClientMessage = self.onClientMessage
         self.client.sendMessage = self.sendMessage
         self.client.cbLog = self.cbLog
-        self.nightWander = NightWander(self.id, config)
+        self.nightWander = NightWander(self.bridge_id)
         self.nightWander.cbLog = self.cbLog
         self.nightWander.client = self.client
         self.nightWander.setNames(idToName2)
